17eac0d62ac4f49038224291103117969f78b4a5
KamenSentai <cvt.alain@gmail.com>
2020-02-24 01:39:10 +0100

    feat(images): import images

---
+++ src/utils/resources.js
@@ -0,0 +1,107 @@
+import AssoieLouvartAbout from '@/images/assoie-louvart/about.jpg'
+import AssoieLouvartCover from '@/images/assoie-louvart/cover.jpg'
+import AssoieLouvartHomeAcneStudios from '@/images/assoie-louvart/home-acne-studios.jpg'
+import AssoieLouvartHomeFeedchi from '@/images/assoie-louvart/home-feedchi.jpg'
+import AssoieLouvartHomeMealthy from '@/images/assoie-louvart/home-mealthy.jpg'
+import AssoieLouvartHomePocketNurse from '@/images/assoie-louvart/home-pocket-nurse.jpg'
+import AssoieLouvartHomeYukey from '@/images/assoie-louvart/home-yukey.jpg'
+import AssoieLouvartProjectAcneStudios from '@/images/assoie-louvart/project-acne-studios.jpg'
+import AssoieLouvartProjectFeedchi from '@/images/assoie-louvart/project-feedchi.jpg'
+import AssoieLouvartProjectMealthy from '@/images/assoie-louvart/project-mealthy.jpg'
+import AssoieLouvartProjectPocketNurse from '@/images/assoie-louvart/project-pocket-nurse.jpg'
+import AssoieLouvartProjectYukey from '@/images/assoie-louvart/project-yukey.jpg'
+
+import ChatMEANCover from '@/images/chatmean/cover.jpg'
+import ChatMEANHome from '@/images/chatmean/home.png'
+import ChatMEANRoom from '@/images/chatmean/room.png'
+import ChatMEANSignIn from '@/images/chatmean/sign-in.png'
+import ChatMEANSignUp from '@/images/chatmean/sign-up.png'
+
+import GaetanLefebvreAboutBlue from '@/images/gaetan-lefebvre/about-blue.jpg'
+import GaetanLefebvreAboutGreen from '@/images/gaetan-lefebvre/about-green.jpg'
+import GaetanLefebvreAboutRed from '@/images/gaetan-lefebvre/about-red.jpg'
+import GaetanLefebvreAboutYellow from '@/images/gaetan-lefebvre/about-yellow.jpg'
+import GaetanLefebvreCover from '@/images/gaetan-lefebvre/cover.jpg'
+import GaetanLefebvreHomeBlue from '@/images/gaetan-lefebvre/home-blue.jpg'
+import GaetanLefebvreHomeGreen from '@/images/gaetan-lefebvre/home-green.jpg'
+import GaetanLefebvreHomeRed from '@/images/gaetan-lefebvre/home-red.jpg'
+import GaetanLefebvreHomeYellow from '@/images/gaetan-lefebvre/home-yellow.jpg'
+import GaetanLefebvreProjectBlue from '@/images/gaetan-lefebvre/project-blue.jpg'
+import GaetanLefebvreProjectGreen from '@/images/gaetan-lefebvre/project-green.jpg'
+import GaetanLefebvreProjectRed from '@/images/gaetan-lefebvre/project-red.jpg'
+import GaetanLefebvreProjectYellow from '@/images/gaetan-lefebvre/project-yellow.jpg'
+
+import MeeroDropCover from '@/images/meerodrop/cover.jpg'
+import MeeroDropDrops from '@/images/meerodrop/drops.jpg'
+import MeeroDropGallery from '@/images/meerodrop/gallery.jpg'
+import MeeroDropGrid from '@/images/meerodrop/grid.jpg'
+import MeeroDropHome from '@/images/meerodrop/home.jpg'
+import MeeroDropProfil from '@/images/meerodrop/profil.jpg'
+import MeeroDropUpload from '@/images/meerodrop/upload.jpg'
+
+import OffitArticle from '@/images/offit/article.jpg'
+import OffitCover from '@/images/offit/cover.jpg'
+import OffitForm from '@/images/offit/form.jpg'
+import OffitHome from '@/images/offit/home.jpg'
+import OffitPrograms from '@/images/offit/programs.jpg'
+import OffitQuote from '@/images/offit/quote.jpg'
+import OffitValues from '@/images/offit/values.jpg'
+
+import ScaneatCover from '@/images/scaneat/cover.jpg'
+import ScaneatEntrance from '@/images/scaneat/entrance.png'
+import ScaneatFeatures from '@/images/scaneat/features.png'
+
+export default {
+  AssoieLouvartAbout,
+  AssoieLouvartCover,
+  AssoieLouvartHomeAcneStudios,
+  AssoieLouvartHomeFeedchi,
+  AssoieLouvartHomeMealthy,
+  AssoieLouvartHomePocketNurse,
+  AssoieLouvartHomeYukey,
+  AssoieLouvartProjectAcneStudios,
+  AssoieLouvartProjectFeedchi,
+  AssoieLouvartProjectMealthy,
+  AssoieLouvartProjectPocketNurse,
+  AssoieLouvartProjectYukey,
+
+  ChatMEANCover,
+  ChatMEANHome,
+  ChatMEANRoom,
+  ChatMEANSignIn,
+  ChatMEANSignUp,
+
+  GaetanLefebvreAboutBlue,
+  GaetanLefebvreAboutGreen,
+  GaetanLefebvreAboutRed,
+  GaetanLefebvreAboutYellow,
+  GaetanLefebvreCover,
+  GaetanLefebvreHomeBlue,
+  GaetanLefebvreHomeGreen,
+  GaetanLefebvreHomeRed,
+  GaetanLefebvreHomeYellow,
+  GaetanLefebvreProjectBlue,
+  GaetanLefebvreProjectGreen,
+  GaetanLefebvreProjectRed,
+  GaetanLefebvreProjectYellow,
+
+  MeeroDropCover,
+  MeeroDropDrops,
+  MeeroDropGallery,
+  MeeroDropGrid,
+  MeeroDropHome,
+  MeeroDropProfil,
+  MeeroDropUpload,
+
+  OffitArticle,
+  OffitCover,
+  OffitForm,
+  OffitHome,
+  OffitPrograms,
+  OffitQuote,
+  OffitValues,
+
+  ScaneatCover,
+  ScaneatEntrance,
+  ScaneatFeatures,
+}
