b3778ac3dbc00cc26a7306b5939b18edbc727147
KamenSentai <cvt.alain@gmail.com>
2020-03-14 22:16:26 +0100

    feat(ui): initialize colors

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div />
+  <div :class="$style.container" />
 </template>
 
 <script>
@@ -9,4 +9,8 @@ export default {
 </script>
 
 <style lang="scss" module>
+.container {
+  width: 100%;
+  height: 100%;
+}
 </style>
