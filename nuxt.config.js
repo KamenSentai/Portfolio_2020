e1ae2de5d1ed55a6087ad0e1a0072bff3f492155
KamenSentai <cvt.alain@gmail.com>
2020-03-16 19:22:48 +0100

    feat(favicon): add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,41 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'msapplication-TileColor', content: '#FFBF00' },
+      { name: 'theme-color', content: '#FFBF00' },
+    ],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+      {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#FFBF00',
+      },
     ],
   },
   loading: {
