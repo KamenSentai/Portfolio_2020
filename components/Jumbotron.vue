c9fb4e41ce2cf51a2076e6a24cf087decd731e0a
KamenSentai <cvt.alain@gmail.com>
2020-03-17 16:38:19 +0100

    feat(jumbotron): build jumbotron component

---
+++ components/Jumbotron.vue
@@ -0,0 +1,23 @@
+<template>
+  <ACVTContainer :class="$style.container">
+    <slot />
+  </ACVTContainer>
+</template>
+
+<script>
+import ACVTContainer from '~/components/Container.vue'
+
+export default {
+  name: 'Jumbotron',
+  components: {
+    ACVTContainer,
+  },
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  height: 100vh;
+  padding-top: calc(#{space(md)} + #{space(lg)} + 1em);
+}
+</style>
