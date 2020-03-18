458cabba7e91d1a39442cc3b02e2bf4723636b05
KamenSentai <cvt.alain@gmail.com>
2020-03-18 19:00:40 +0100

    feat(icon): add social networks icons

--- components/Icons/index.js
+++ components/Icons/index.js
@@ -1,9 +1,17 @@
 import ArrowDown from './ArrowDown.vue'
 import ArrowUp from './ArrowUp.vue'
+import Email from './Email.vue'
+import GitHub from './GitHub.vue'
+import LinkedIn from './LinkedIn.vue'
 import Mouse from './Mouse.vue'
+import Twitter from './Twitter.vue'
 
 export default {
   ArrowDown,
   ArrowUp,
+  Email,
+  GitHub,
+  LinkedIn,
   Mouse,
+  Twitter,
 }
