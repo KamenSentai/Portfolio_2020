3f4dd8bae646fffebff141b80337ba6462a8a6e1
KamenSentai <cvt.alain@gmail.com>
2020-03-18 23:09:37 +0100

    feat(text): build text component

---
+++ components/Text.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="$style.container">
+    <p
+      v-for="(paragraph, i) in text"
+      :key="`paragraph-${i}`"
+    >
+      {{ paragraph }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Text',
+  props: {
+    text: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  display: grid;
+  grid-gap: space(sx);
+  align-content: flex-start;
+  font-weight: 300;
+  line-height: 1.25;
+}
+</style>
